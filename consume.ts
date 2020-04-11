2c4da0c915c02c67e397648c04c6e4dee45e6e15
erikoleary <erik.m.oleary@gmail.com>
2020-04-11 15:39:10 -0500

    Updated sample

--- consume.ts
+++ consume.ts
@@ -1,3 +1,9 @@
 import * as api from './output';
 
-const x = await api.Leads.get("/api/{partner}/Leads", { partner: "Default" });
+var leads = await api.Leads.get(
+    "/api/{partner}/Leads", 
+    { partner: "Default" });
+
+var profile = await api.Account.get("/api/User/Profile");
+
+var users = await api.Users.get("/api/{partner}/Users", { partner: "Default" });
